cbc9414a0af8456b5847f67242b216a0340464c2
SHUJA <106554018+Shujaulislam@users.noreply.github.com>
2024-09-02 21:45:35 +0530

    logos and icons added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,12 @@ export default function RootLayout({
   
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/32x32.ico"></link>
+        <link rel="apple-touch-icon" sizes="180x180" href="/HumayaBirdLogo.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/32x32.ico" />
+        <link rel="icon" type="image/png" sizes="16x16" href="/16x16.ico" />
+      </head>
    <body className={`${syne.className} ${roboto.className} ${montserrat.className} ${nunito.className} ${inter.className}`}>
     <Navbar/>
     {children}
